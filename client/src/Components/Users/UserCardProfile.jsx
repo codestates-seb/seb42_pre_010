2648c6e3fbfb884841d081a3e0c01bcfe0edc03e
Mia-Oh <songmiaoh@gmail.com>
2023-02-19 20:55:45 +0900

    feat:UserCardProfile- Stats Section #60

--- client/src/Components/Users/UserCardProfile.jsx
+++ client/src/Components/Users/UserCardProfile.jsx
@@ -1,7 +1,56 @@
-//import styled from 'styled-components';
+import styled from 'styled-components';
+
+const UserCardProfileWrap = styled.div`
+  display: flex;
+`;
+
+const ProfileContentRightSection = styled.div`
+  border: 1px solid black;
+  background-color: pink;
+  width: 100%;
+  height: 500px;
+`;
+
+export const ProfileStatsSection = () => {
+  const ProfileStatsWrap = styled.div`
+    width: 215px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 60px);
+    background-color: brown;
+    div {
+      border: 1px solid black;
+    }
+  `;
+  return (
+    <>
+      <h2>Stats</h2>
+      <ProfileStatsWrap>
+        <div>box1</div>
+        <div>box2</div>
+        <div>box3</div>
+        <div>box4</div>
+      </ProfileStatsWrap>
+    </>
+  );
+};
+
+const ProfileContentLeftSection = styled.div`
+  border: 1px solid black;
+  width: 100%;
+  height: 500px;
+  background-color: blue;
+`;
 
 const UserCardProfile = () => {
-  return <h1>UserCardProfile</h1>;
+  return (
+    <UserCardProfileWrap>
+      <ProfileContentRightSection>
+        <ProfileStatsSection />
+      </ProfileContentRightSection>
+      <ProfileContentLeftSection></ProfileContentLeftSection>
+    </UserCardProfileWrap>
+  );
 };
 
 export default UserCardProfile;
