4f31c15a492e4c811d27588c2dbbf0a95300befd
suming <ipower_overi@naver.com>
2023-02-20 11:53:41 +0900

    feat: dummydata(initialState)를 활용하여 users목록 페이지 틀 구현 #66

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from '.';
 import Header from './Components/Header/Header';
 import Nav from './Components/Nav/Nav';
-import Sidebar from './Components/Sidebar/Sidebar';
+// import Sidebar from './Components/Sidebar/Sidebar';
 import Footer from './Components/Footer/Footer';
+import Users from './Pages/Users';
 import styled from 'styled-components';
 
 const MainContainer = styled.div`
@@ -20,7 +21,7 @@ function App() {
         <Routes>
           <Route />
         </Routes>
-        <Sidebar />
+        <Users />
       </MainContainer>
       <Footer />
     </>
