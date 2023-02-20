a4ac9a55f003f3007c377d67ec6230c7f7b4f1f5
Mia-Oh <songmiaoh@gmail.com>
2023-02-20 11:49:33 +0900

    style:UserCard CSS #59

--- client/src/Components/Users/UserCard.jsx
+++ client/src/Components/Users/UserCard.jsx
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 import UserCardProfile from './UserCardProfile';
+import { MdCake, MdLocationOn } from 'react-icons/md';
+import { FiClock } from 'react-icons/fi';
+import { BiCalendar } from 'react-icons/bi';
+import { AiFillGithub } from 'react-icons/ai';
+import { BsTwitter } from 'react-icons/bs';
+import { FaPen } from 'react-icons/fa';
 
 const getRandomNumber = (min, max) => {
   return parseInt(Math.random() * (Number(max) - Number(min) + 2));
@@ -39,46 +45,83 @@ const UserCardContainer = styled.main`
   max-width: 1100px;
   height: 500px;
   width: calc(100% - 164px);
-  border: 1px solid black;
   position: relative;
   list-style: none;
+  svg {
+    margin-right: 5px;
+  }
 `;
 
 const UserCardInfoBlock = styled.div`
-  border: 1px solid purple;
   display: flex;
   align-items: center;
 `;
 
 const UserCardImg = styled.img.attrs(() => ({
   src: `${userimg}`,
-}))``;
+}))`
+  width: 130px;
+  height: 130px;
+  margin: 16px;
+  border-radius: 5px;
+`;
 
 const UserCardInfoContnet = styled.div`
-  background-color: #86ddff76;
+  h1 {
+    font-size: 2.6rem;
+    margin-bottom: 12px;
+  }
+`;
+
+const UserCardInfoListWrap = styled.ul`
+  display: flex;
+  color: hsl(210, 8%, 45%);
+  margin: 8px;
+  font-size: 20px;
 `;
 
 const UserCardInfoList = styled.ul`
-  border: 1px solid yellow;
   display: flex;
+  align-items: center;
+  li {
+    margin-right: 10px;
+    font-size: 14px;
+  }
 `;
 
 const UserCardButtonWrap = styled.div`
-  border: 1px solid blue;
+  display: flex;
   position: absolute;
   right: 0;
   top: 0;
+  button {
+    height: 37px;
+    text-decoration: none;
+    margin: 0 3px;
+    background-color: white;
+    color: hsl(210, 8%, 45%);
+    border: 1px solid;
+    cursor: pointer;
+    font-size: 0.8rem;
+    border-radius: 3px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    padding: 15px;
+    &:hover {
+      background-color: #f8f9f9;
+    }
+  }
 `;
 
 const UserCardNavSection = styled.div`
-  border: 1px solid black;
   display: flex;
 `;
 
 const UserCardNavList = styled.li``;
 
 const UserCardConentSection = styled.div`
-  background-color: blanchedalmond;
   width: 100%;
   height: 100%;
 `;
@@ -94,22 +137,33 @@ export const UserCard = () => {
         <UserCardImg />
         <UserCardInfoContnet>
           <h1>Youngk</h1>
-          <UserCardInfoList>
-            <li>Member for 6 days</li>
-            <li>Last seen this week</li>
-            <li>Visited 6 days, 6 consecutive</li>
-          </UserCardInfoList>
-          <UserCardInfoList>
-            <li>트위터</li>
-            <li>깃허브</li>
-            <li>개인 링크</li>
-            <li>위치</li>
-          </UserCardInfoList>
+          <UserCardInfoListWrap>
+            <UserCardInfoList>
+              <MdCake />
+              <li>Member for 6 days</li>
+            </UserCardInfoList>
+            <UserCardInfoList>
+              <FiClock />
+              <li>Last seen this week</li>
+            </UserCardInfoList>
+            <UserCardInfoList>
+              <BiCalendar />
+              <li>Visited 6 days, 6 consecutive</li>
+            </UserCardInfoList>
+          </UserCardInfoListWrap>
+          <UserCardInfoListWrap>
+            <BsTwitter />
+            <AiFillGithub />
+            <MdLocationOn />
+          </UserCardInfoListWrap>
         </UserCardInfoContnet>
       </UserCardInfoBlock>
       <UserCardButtonWrap>
-        <button>Edit profile</button>
-        <button>Network profile</button>
+        <button>
+          <FaPen />
+          Edit profile
+        </button>
+        <button>Profiles</button>
       </UserCardButtonWrap>
       <UserCardNavSection>
         {usercardNav.map((ele, idx) => {
