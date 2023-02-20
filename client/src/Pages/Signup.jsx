cec0e92b15d6553198a457b9693df8597eb28154
Mia-Oh <songmiaoh@gmail.com>
2023-02-20 23:53:30 +0900

    feat:SignupInputSection 중간 구현 #75

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -45,6 +45,102 @@ const SignupFormSection = styled.div`
   border: 1px solid black;
   width: 440px;
 `;
+const SignupSocialWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 16px;
+`;
+
+const SignupGoogle = styled.button`
+  width: 300px;
+  padding: 10px;
+  margin: 4px 0px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  border: 1px solid hsl(210, 8%, 85%);
+`;
+
+const BtnText = styled.span`
+  margin-left: 5px;
+`;
+
+const SignupGithub = styled(SignupGoogle)`
+  background-color: #2f3338;
+  color: white;
+`;
+
+const SignupFacebook = styled(SignupGithub)`
+  background-color: #365499;
+`;
+
+const SignupInputWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 300px;
+  padding: 24px;
+  background-color: white;
+  border-radius: 7px;
+  box-shadow: rgb(0 0 0 / 5%) 0px 10px 24px, rgb(0 0 0 / 5%) 0px 20px 48px,
+    rgb(0 0 0 / 10%) 0px 1px 4px;
+  margin-bottom: 24px;
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const SignupInputBlock = styled.div`
+  margin: 6px 0;
+`;
+
+const SignupInputTitle = styled.div`
+  padding: 0 2px;
+  font-weight: bold;
+`;
+
+const TextInput = styled.input`
+  width: 100%;
+  margin-top: 5px;
+  padding: 0.6em 0.7em;
+  border: 1px solid hsl(210, 8%, 75%);
+  border-radius: 3px;
+  color: hsl(210, 8%, 5%);
+  font-size: 13px;
+  font-family: inherit;
+  position: relative;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Requisition = styled.p`
+  color: hsl(210, 8%, 45%);
+  font-size: 12px;
+  margin-bottom: 4px;
+  margin-top: 4px;
+`;
+
+const TestBlock = styled.div`
+  margin: 6px 0;
+  padding: 8px 0px 2px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: hsl(210, 8%, 95%);
+  border-color: hsl(210, 8%, 90%);
+  border-radius: 3px;
+  border-width: 1px;
+`;
+
+const RecaptchaBox = styled.div`
+  width: 164px;
+  height: 144px;
+`;
 
 const Signup = () => {
   return (
@@ -122,7 +218,82 @@ const Signup = () => {
           Get Stack Overflow for Teams free for up to 50 users.
         </a>
       </SignupInfoSection>
-      <SignupFormSection />
+      <SignupFormSection>
+        <SignupSocialWrap>
+          <SignupGoogle>
+            <svg aria-hidden="true" width="18" height="18" viewBox="0 0 18 18">
+              <path
+                d="M16.51 8H8.98v3h4.3c-.18 1-.74 1.48-1.6 2.04v2.01h2.6a7.8 7.8 0 0 0 2.38-5.88c0-.57-.05-.66-.15-1.18Z"
+                fill="#4285F4"
+              ></path>
+              <path
+                d="M8.98 17c2.16 0 3.97-.72 5.3-1.94l-2.6-2a4.8 4.8 0 0 1-7.18-2.54H1.83v2.07A8 8 0 0 0 8.98 17Z"
+                fill="#34A853"
+              ></path>
+              <path
+                d="M4.5 10.52a4.8 4.8 0 0 1 0-3.04V5.41H1.83a8 8 0 0 0 0 7.18l2.67-2.07Z"
+                fill="#FBBC05"
+              ></path>
+              <path
+                d="M8.98 4.18c1.17 0 2.23.4 3.06 1.2l2.3-2.3A8 8 0 0 0 1.83 5.4L4.5 7.49a4.77 4.77 0 0 1 4.48-3.3Z"
+                fill="#EA4335"
+              ></path>
+            </svg>
+            <BtnText>Sign up with Google</BtnText>
+          </SignupGoogle>
+          <SignupGithub>
+            <svg aria-hidden="true" width="18" height="18" viewBox="0 0 18 18">
+              <path
+                d="M9 1a8 8 0 0 0-2.53 15.59c.4.07.55-.17.55-.38l-.01-1.49c-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82a7.42 7.42 0 0 1 4 0c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48l-.01 2.2c0 .21.15.46.55.38A8.01 8.01 0 0 0 9 1Z"
+                fill="#fff"
+              ></path>
+            </svg>
+            <BtnText>Log in with GitHub</BtnText>
+          </SignupGithub>
+          <SignupFacebook>
+            <svg aria-hidden="true" width="18" height="18" viewBox="0 0 18 18">
+              <path
+                d="M3 1a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H3Zm6.55 16v-6.2H7.46V8.4h2.09V6.61c0-2.07 1.26-3.2 3.1-3.2.88 0 1.64.07 1.87.1v2.16h-1.29c-1 0-1.19.48-1.19 1.18V8.4h2.39l-.31 2.42h-2.08V17h-2.5Z"
+                fill="#fff"
+              ></path>
+            </svg>
+            <BtnText>Log in with Facebook</BtnText>
+          </SignupFacebook>
+        </SignupSocialWrap>
+        <SignupInputWrap>
+          <SignupInputBlock>
+            <SignupInputTitle>Display name</SignupInputTitle>
+            <TextInput />
+          </SignupInputBlock>
+          <SignupInputBlock>
+            <SignupInputTitle>Email</SignupInputTitle>
+            <TextInput />
+          </SignupInputBlock>
+          <SignupInputBlock>
+            <SignupInputTitle>Password</SignupInputTitle>
+            <TextInput />
+            <Requisition>
+              Passwords must contain at least eight characters, including at
+              least 1 letter and 1 number.
+            </Requisition>
+          </SignupInputBlock>
+          <TestBlock>
+            <RecaptchaBox>
+              <iframe
+                title="reCAPTCHA"
+                src="https://www.google.com/recaptcha/api2/anchor?ar=2&amp;k=6Lfmm70ZAAAAADvPzM6OhZ8Adi40-78E-aYfc1ZS&amp;co=aHR0cHM6Ly9zdGFja292ZXJmbG93LmNvbTo0NDM.&amp;hl=en&amp;v=5qcenVbrhOy8zihcc2aHOWD4&amp;size=compact&amp;cb=3unij7lmwk0y"
+                width="164"
+                height="144"
+                role="presentation"
+                name="a-9911dfsh95jz"
+                frameBorder="0"
+                scrolling="no"
+                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"
+              ></iframe>
+            </RecaptchaBox>
+          </TestBlock>
+        </SignupInputWrap>
+      </SignupFormSection>
     </Signupwrap>
   );
 };
