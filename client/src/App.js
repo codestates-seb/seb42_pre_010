d39298b79bf927ddbba38bba406fd482aa3b1099
Mia-Oh <songmiaoh@gmail.com>
2023-02-20 17:44:49 +0900

    style: 전역 스타일 추가

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,19 @@ import { GlobalStyle } from '.';
 import Header from './Components/Header/Header';
 import Nav from './Components/Nav/Nav';
 import Footer from './Components/Footer/Footer';
+import UserCard from './Components/Users/UserCard';
 import styled from 'styled-components';
 
+//수정 함@16:28
 const MainContainer = styled.div`
   display: flex;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  width: 1100px;
+  max-width: 1100px;
+  height: 100%;
 `;
 
 function App() {
@@ -16,9 +25,12 @@ function App() {
       <Header />
       <MainContainer>
         <Nav />
-        <Routes>
-          <Route />
-        </Routes>
+        <Content>
+          <Routes>
+            {/* 출력 테스트용 임의 작성 */}
+            <Route path="/" element={<UserCard />} />
+          </Routes>
+        </Content>
       </MainContainer>
       <Footer />
     </>
