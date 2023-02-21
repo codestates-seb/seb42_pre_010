5abf635c54dace5b258c23a730f4c8d8c4748e21
Mia-Oh <songmiaoh@gmail.com>
2023-02-21 15:07:50 +0900

    chore:RouterProvider를 사용한 페이지별 레이아웃 설정 #54

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,35 @@
-import { Route, Routes } from 'react-router-dom';
-import { GlobalStyle } from '.';
-import Header from './Components/Header/Header';
-import Nav from './Components/Nav/Nav';
-import Sidebar from './Components/Sidebar/Sidebar';
-import Footer from './Components/Footer/Footer';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Root, WithoutRoot } from './GlobalStyle';
+//import Sidebar from './Components/Sidebar/Sidebar';
+import Login from './Pages/Login/Login';
+import Signin from './Pages/Signup/Signup';
+import AskQuestion from './Pages/AskQuestion/AskQuestion';
+import Tag from './Pages/Tag/Tag';
 import UserCard from './Components/Users/UserCard';
-import styled from 'styled-components';
 
-//수정 함@16:28
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const Content = styled.div`
-  width: 1100px;
-  max-width: 1100px;
-  height: 100%;
-`;
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      { index: true, element: <UserCard /> }, // index가 true인 컴포넌트는 Root의 Outlet에 기본으로 보여짐
+      { path: '/users', element: <UserCard /> },
+      { path: '/tags', element: <Tag /> },
+    ],
+  },
+  {
+    path: '/',
+    element: <WithoutRoot />, // Nav, Footer, Sidebar가 없는 페이지
+    children: [
+      { path: '/users/login', element: <Login /> },
+      { path: '/users/signup', element: <Signin /> },
+      { path: '/questions/ask', element: <AskQuestion /> },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <Header />
-      <MainContainer>
-        <Nav />
-        <Routes>
-          <Route />
-        </Routes>
-        <Sidebar />
-      </MainContainer>
-      <Footer />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/GlobalStyle.jsx
@@ -0,0 +1,97 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import { Outlet } from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Nav from './Components/Nav/Nav';
+//import Sidebar from './Components/Sidebar/Sidebar';
+import Footer from './Components/Footer/Footer';
+
+export const GlobalStyle = createGlobalStyle`
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+`;
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+export const WithoutRootConent = styled.div`
+  margin-top: 2px;
+`;
+
+export const Root = () => {
+  return (
+    <div>
+      <Header />
+      <MainContainer>
+        <Nav />
+        <Content>
+          <Outlet />
+        </Content>
+      </MainContainer>
+      <Footer />
+    </div>
+  );
+};
+
+export const WithoutRoot = () => {
+  return (
+    <div>
+      <Header />
+      <WithoutRootConent>
+        <Outlet />
+      </WithoutRootConent>
+    </div>
+  );
+};
