3d6ea5be6c075aaddde162e834dd51acb3a557c3
Mia-Oh <songmiaoh@gmail.com>
2023-02-21 22:10:19 +0900

    feat:Signup,Login-유효성 검사 추가(style 제외) #74 #75

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   SocialGoogleSvg,
   SocialGithubSvg,
@@ -25,6 +26,53 @@ import {
 const REACT_APP_URL = 'http://localhost:3000';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [emailMsg, setEmailMsg] = useState('');
+  const [passwordMsg, setPasswordMsg] = useState('');
+
+  // 이메일 정규 표현식
+  const validateEmail = (email) => {
+    return email
+      .toLowerCase()
+      .match(
+        /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+      );
+  };
+
+  // 비밀번호 정규 표현식
+  const validatePwd = (password) => {
+    return password
+      .toLowerCase()
+      .match(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{10,25}$/);
+  };
+
+  // 이메일
+  const onChangeEmail = (e) => {
+    const currEmail = e.target.value;
+    setEmail(currEmail);
+
+    if (!validateEmail(email)) {
+      setEmailMsg(`${email} is not a valid email address.`);
+    } else {
+      setEmailMsg('');
+    }
+  };
+
+  // 비밀번호
+  const onChangePassword = (e) => {
+    const currPassword = e.target.value;
+    setPassword(currPassword);
+
+    if (!validatePwd(password)) {
+      setPasswordMsg(
+        `Please add one of the following things to make your password stronger:`
+      );
+    } else {
+      setPasswordMsg('');
+    }
+  };
   return (
     <LoginWrap>
       <LoginLogo>
@@ -47,12 +95,14 @@ const Login = () => {
       <LoginInputWrap>
         <LoginInputBlock>
           <LoginInputTitle>Email</LoginInputTitle>
-          <TextInput />
+          <TextInput onChange={onChangeEmail} />
         </LoginInputBlock>
+        <h1>{emailMsg}</h1>
         <LoginInputBlock>
           <LoginInputTitle>Password</LoginInputTitle>
-          <TextInput />
+          <TextInput onChange={onChangePassword} />
         </LoginInputBlock>
+        <h1>{passwordMsg}</h1>
         <LoginSubmitBtn type="submit">Log in</LoginSubmitBtn>
       </LoginInputWrap>
       <MoreInfoWrap>
--- client/src/Pages/Signup/Signup.jsx
+++ client/src/Pages/Signup/Signup.jsx
@@ -32,22 +32,53 @@ import {
 } from '../../Components/Signup/SignupStyle';
 
 const Signup = () => {
+  //const [nickname, setNickname] = useState('');
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  //이메일 유효성 검사 함수
-  // const validateEmail = (email) => {
-  //   return email
-  //     .toLowerCase()
-  //     .match(
-  //       /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-  //     );
-  // };
+  const [emailMsg, setEmailMsg] = useState('');
+  const [passwordMsg, setPasswordMsg] = useState('');
 
-  //이메일
+  // 이메일 정규 표현식
+  const validateEmail = (email) => {
+    return email
+      .toLowerCase()
+      .match(
+        /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+      );
+  };
+
+  // 비밀번호 정규 표현식
+  const validatePwd = (password) => {
+    return password
+      .toLowerCase()
+      .match(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{10,25}$/);
+  };
+
+  // 이메일
   const onChangeEmail = (e) => {
     const currEmail = e.target.value;
     setEmail(currEmail);
-    console.log(email);
+
+    if (!validateEmail(email)) {
+      setEmailMsg(`${email} is not a valid email address.`);
+    } else {
+      setEmailMsg('');
+    }
+  };
+
+  // 비밀번호
+  const onChangePassword = (e) => {
+    const currPassword = e.target.value;
+    setPassword(currPassword);
+
+    if (!validatePwd(password)) {
+      setPasswordMsg(
+        `Please add one of the following things to make your password stronger:`
+      );
+    } else {
+      setPasswordMsg('');
+    }
   };
 
   return (
@@ -79,9 +110,11 @@ const Signup = () => {
             <SignupInputTitle>Email</SignupInputTitle>
             <TextInput onChange={onChangeEmail} />
           </SignupInputBlock>
+          <h1>{emailMsg}</h1>
           <SignupInputBlock>
             <SignupInputTitle>Password</SignupInputTitle>
-            <TextInput />
+            <TextInput onChange={onChangePassword} />
+            <h1>{passwordMsg}</h1>
             <Requisition>
               Passwords must contain at least eight characters, including at
               least 1 letter and 1 number.
