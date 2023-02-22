5f18aeee2e3b56c3617f8f6f3d1cae365aa30299
Mia-Oh <songmiaoh@gmail.com>
2023-02-22 14:06:56 +0900

    chore:제작 기간 동안 사용할 dummydata 저장 폴더 생성 #68

---
+++ client/src/data/userList.js
@@ -0,0 +1,74 @@
+const userList = [
+  {
+    id: 0,
+    userName: 'Jae',
+    createdAt: '2023.02.16 14:00:00',
+    questionCount: 23,
+    answerCount: 45,
+    picture: `https://randomuser.me/api/portraits/women/${getRandomNumber(
+      1,
+      98
+    )}.jpg`,
+  },
+  {
+    id: 1,
+    userName: 'wonpil',
+    createdAt: '2023.02.16 14:00:00',
+    questionCount: 23,
+    answerCount: 45,
+    picture: `https://randomuser.me/api/portraits/women/${getRandomNumber(
+      1,
+      98
+    )}.jpg`,
+  },
+  {
+    id: 2,
+    userName: 'Young K',
+    createdAt: '2023.02.16 14:00:00',
+    questionCount: 3,
+    answerCount: 4,
+    picture: `https://randomuser.me/api/portraits/men/${getRandomNumber(
+      1,
+      98
+    )}.jpg`,
+  },
+  {
+    id: 3,
+    userName: 'Do Woon',
+    createdAt: '2023.02.16 14:00:00',
+    questionCount: 3,
+    answerCount: 4,
+    picture: `https://randomuser.me/api/portraits/men/${getRandomNumber(
+      1,
+      98
+    )}.jpg`,
+  },
+  {
+    id: 4,
+    userName: 'Sung Jin',
+    createdAt: '2023.02.16 14:00:00',
+    questionCount: 36,
+    answerCount: 4,
+    picture: `https://randomuser.me/api/portraits/men/${getRandomNumber(
+      1,
+      98
+    )}.jpg`,
+  },
+  {
+    id: 6,
+    userName: 'Sung Jin',
+    createdAt: '2023.02.16 14:00:00',
+    questionCount: 36,
+    answerCount: 4,
+    picture: `https://randomuser.me/api/portraits/men/${getRandomNumber(
+      1,
+      98
+    )}.jpg`,
+  },
+];
+
+const getRandomNumber = (min, max) => {
+  return parseInt(Math.random() * (Number(max) - Number(min) + 2));
+};
+
+export default userList;
