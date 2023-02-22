fbca5179288fe694a265d04f09c7ee3c118d1bfb
youa7878 <dkgus7123@naver.com>
2023-02-23 01:51:48 +0900

    style: 버튼 style 수정 #122

--- client/src/Components/Pagination.jsx
+++ client/src/Components/Pagination.jsx
@@ -1,17 +1,53 @@
 import styled from 'styled-components';
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { RxDoubleArrowLeft, RxDoubleArrowRight } from 'react-icons/rx';
 
 const PaginationBlock = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  margin-top: 30px;
 
   .icon {
-    width: 20px;
-    height: 20px;
-    margin: 5px;
+    /* background: ; */
+    border: 1px solid #c4c4c497;
+    color: #0a95ff;
   }
+
+  .icon:hover {
+    color: #17649e;
+
+    &.bg {
+      background-color: #dddddd2b;
+    }
+  }
+
+  .back {
+    border-top-left-radius: 3px;
+    border-bottom-left-radius: 3px;
+  }
+
+  .next {
+    border-top-right-radius: 3px;
+    border-bottom-right-radius: 3px;
+  }
+
+  button {
+    padding: 5px;
+    width: 30px;
+    height: 30px;
+    border: 0;
+    outline: 0;
+    background-color: transparent;
+  }
+`;
+
+const IconBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 25px;
+  height: 28.5px;
 `;
 
 const Pagination = ({ postPerPage, totalPosts, paginate, currentPage }) => {
@@ -22,21 +58,29 @@ const Pagination = ({ postPerPage, totalPosts, paginate, currentPage }) => {
   }
   return (
     <PaginationBlock>
-      <IoIosArrowBack
-        className="icon"
+      <IconBlock
+        className="icon back bg"
         onClick={() => paginate(currentPage - 1)}
-      />
+      >
+        <RxDoubleArrowLeft size={12} />
+      </IconBlock>
       <div className="pagination">
         {pageNumbers.map((number) => (
-          <button key={number} onClick={() => paginate(number)}>
+          <button
+            key={number}
+            className="icon bg"
+            onClick={() => paginate(number)}
+          >
             {number}
           </button>
         ))}
       </div>
-      <IoIosArrowForward
-        className="icon"
+      <IconBlock
+        className="icon next bg"
         onClick={() => paginate(currentPage + 1)}
-      />
+      >
+        <RxDoubleArrowRight size={12} />
+      </IconBlock>
     </PaginationBlock>
   );
 };
