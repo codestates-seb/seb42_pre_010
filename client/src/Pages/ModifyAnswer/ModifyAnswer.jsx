4b4b85e9e4eee8bdb94e8fbf20c779818711b255
youa7878 <dkgus7123@naver.com>
2023-02-22 14:42:04 +0900

    feat: 버튼 추가 #110

--- client/src/Pages/ModifyAnswer/ModifyAnswer.jsx
+++ client/src/Pages/ModifyAnswer/ModifyAnswer.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import { WritingBodyForm } from '../../Components/AskForm/WritingForm';
 import questionsData from '../Questions/QuestionsDummyData';
+import EditButton from '../../Components/Button/EditButton';
 
 const ModifyAnswerContainer = styled.div`
   display: flex;
@@ -9,26 +10,28 @@ const ModifyAnswerContainer = styled.div`
   width: 1200px;
 `;
 
-const ModifyAnswerBlock = styled.div`
-  width: 700px;
+const ModifyAnsweyrWrap = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+const ModifyAnswerBlock = styled.div`
+  width: 700px;
   padding-left: 24px;
 `;
 
 const QuestionPost = styled.div`
-  /* background: red; */
   margin-top: 30px;
   max-height: 300px;
   overflow: scroll;
 
-  .QuestionTitle {
+  .questionTitle {
     font-size: 18px;
     color: #0074d2;
     margin: 5px 0px 20px 0px;
   }
 
-  .QuestionContent {
+  .questionContent {
     font-size: 14px;
     line-height: 1.5em;
     margin-bottom: 10px;
@@ -42,7 +45,6 @@ const WritingFormBlock = styled.div`
 
   .title {
     font-size: 18px;
-    /* font-weight: bolder; */
     margin: 10px 0px 20px 0px;
   }
 `;
@@ -54,18 +56,25 @@ const ModifyAnswer = () => {
     setValue(value);
   };
 
+  const handleOnCancel = () => {
+    window.location.href = './questions';
+  };
+
   return (
     <ModifyAnswerContainer>
-      <ModifyAnswerBlock>
-        <QuestionPost>
-          <h3 className="QuestionTitle">{questionsData[0].title}</h3>
-          <div className="QuestionContent">{questionsData[0].content}</div>
-        </QuestionPost>
-        <WritingFormBlock>
-          <h3 className="title">Answer</h3>
-          <WritingBodyForm value={value} handleValue={handleValue} />
-        </WritingFormBlock>
-      </ModifyAnswerBlock>
+      <ModifyAnsweyrWrap>
+        <ModifyAnswerBlock>
+          <QuestionPost>
+            <h3 className="questionTitle">{questionsData[0].title}</h3>
+            <div className="questionContent">{questionsData[0].content}</div>
+          </QuestionPost>
+          <WritingFormBlock>
+            <h3 className="title">Answer</h3>
+            <WritingBodyForm value={value} handleValue={handleValue} />
+          </WritingFormBlock>
+        </ModifyAnswerBlock>
+        <EditButton className="button" handleOnCancel={handleOnCancel} />
+      </ModifyAnsweyrWrap>
     </ModifyAnswerContainer>
   );
 };
