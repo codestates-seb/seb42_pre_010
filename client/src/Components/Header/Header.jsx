a507751645d29a80ddf362847c5f3f2e942c885e
Mia-Oh <songmiaoh@gmail.com>
2023-02-22 13:05:15 +0900

    refactor:Header-style 별도 관리 #50

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,115 +1,32 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  HeaderBlock,
+  PubHeaderNavWrap,
+  LoggedHeaderNavWrap,
+  PubHeaderContentWrap,
+  MypageWrap,
+  MessageBlock,
+  AchievmentBlock,
+  HelpBlock,
+  CommunityBlock,
+  LoginBlock,
+  SignupBlock,
+  LoggedHeaderContentWrap,
+  LogoBlock,
+} from './HeaderStyle';
 
-import styled from 'styled-components';
 import { FiSearch } from 'react-icons/fi';
 import { ImDrawer2 } from 'react-icons/im';
 import { RiTrophyFill } from 'react-icons/ri';
 import { BsQuestionCircleFill } from 'react-icons/bs';
 import { FaStackExchange } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 
 const REACT_APP_URL = 'http://localhost:3000';
 
-export const HeaderBlock = styled.div`
-  width: 100%;
-  height: 50px;
-  position: inherit; // 우선은 스티키 사용하지 않음
-  top: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-top: 3px solid hsl(27, 90%, 55%);
-  box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
-    0 2px 8px hsla(0, 0%, 0%, 0.05);
-  background-color: #ffffff;
-  nav {
-    width: 98rem;
-    height: 100%;
-    max-width: 1264px;
-    margin: 0 auto;
-    display: flex;
-    align-items: center;
-  }
-  form {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    border: 1px solid lightgray;
-    border-radius: 3px;
-    padding: 7px 9px;
-    min-width: 184px;
-    flex-grow: 1;
-    margin: 0 8px;
-    background-color: white;
-  }
-  input {
-    padding-left: 1rem;
-    width: 100%;
-    height: 100%;
-    border: none;
-    outline: none;
-    font-size: 80%;
-  }
-  li {
-    list-style: none;
-  }
-  a {
-    text-decoration: none;
-  }
-`;
-
-//! About, Products, For Teams || Products
 export const HeaderNav = (logged) => {
-  // console.log(logged.logged);
-
-  return <>{logged.logged ? <PubHeaderNav /> : <LoggedHeaderNav />}</>;
+  return <>{logged.logged ? <LoggedHeaderNav /> : <PubHeaderNav />}</>;
 };
-
-// About, Products, For Teams
-export const PubHeaderNavWrap = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  width: 226px;
-  margin: 0 10px;
-  li {
-    color: hsl(210, 8%, 35%);
-    text-decoration: none;
-    border: none;
-    padding: 8.5px 12px;
-    margin: 0px;
-    font-size: 13px;
-    :hover {
-      color: black;
-      border-radius: 1000px;
-      background-color: hsl(210, 8%, 90%);
-    }
-  }
-`;
-
-export const LoggedHeaderNavWrap = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  margin: 0 10px;
-  li {
-    color: hsl(210, 8%, 35%);
-    text-decoration: none;
-    border: none;
-    padding: 8.5px 12px;
-    margin: 0px;
-    font-size: 13px;
-    :hover {
-      color: black;
-      border-radius: 1000px;
-      background-color: hsl(210, 8%, 90%);
-    }
-  }
-`;
 
 export const PubHeaderNav = () => {
   return (
@@ -121,7 +38,6 @@ export const PubHeaderNav = () => {
   );
 };
 
-// Products
 export const LoggedHeaderNav = () => {
   return (
     <LoggedHeaderNavWrap>
@@ -130,7 +46,6 @@ export const LoggedHeaderNav = () => {
   );
 };
 
-// 검색 Input 창
 export const SearchBlock = () => {
   return (
     <form>
@@ -140,48 +55,13 @@ export const SearchBlock = () => {
   );
 };
 
-//! 로그인, 회원 가입 || 마이페이지
 export const HeaderContent = (logged) => {
-  //console.log(logged.logged);
-
   return <>{logged.logged ? <LoggedHeaderContent /> : <PubHeaderContent />}</>;
 };
 
-export const PubHeaderContentWrap = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  li {
-    padding: 15px;
-    :hover {
-      background-color: hsl(210, 8%, 90%);
-      color: #191919;
-    }
-  }
-  svg {
-    font-size: 20px;
-    color: hsl(210, 8%, 35%);
-    :hover {
-      color: #191919;
-    }
-  }
-`;
-
-// 마이페이지
-export const MypageWrap = styled.li``;
-
-export const MessageBlock = styled.li``;
-
-export const AchievmentBlock = styled.li``;
-
-export const HelpBlock = styled.li``;
-
-export const CommunityBlock = styled.li``;
-
-export const PubHeaderContent = () => {
+export const LoggedHeaderContent = () => {
   return (
-    <PubHeaderContentWrap>
+    <LoggedHeaderContentWrap>
       <MypageWrap></MypageWrap>
       <MessageBlock>
         <ImDrawer2 />
@@ -195,79 +75,26 @@ export const PubHeaderContent = () => {
       <CommunityBlock>
         <FaStackExchange />
       </CommunityBlock>
-    </PubHeaderContentWrap>
+    </LoggedHeaderContentWrap>
   );
 };
 
-// 로그인, 회원 가입
-export const LoginBlock = styled.li`
-  color: hsl(205, 47%, 42%);
-  border: 1px solid hsl(205, 41%, 63%);
-  background-color: hsl(205, 46%, 92%);
-  :hover {
-    background-color: hsl(205, 41%, 63%);
-  }
-  .logtxt {
-    color: hsl(205, 47%, 42%);
-  }
-`;
-
-export const SignupBlock = styled.li`
-  color: hsl(205, 46%, 92%);
-  border: 1px solid hsl(205, 41%, 63%);
-  background-color: hsl(206, 100%, 52%);
-  :hover {
-    background-color: hsl(206, 100%, 40%);
-  }
-  .signtxt {
-    color: white;
-  }
-`;
-
-export const LoggedHeaderContentWrap = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  li {
-    border-radius: 3px;
-    margin: 3px;
-    padding: 9.5px 12px;
-    font-size: 0.8em;
-  }
-`;
-
-export const LoggedHeaderContent = () => {
+export const PubHeaderContent = () => {
   return (
-    <LoggedHeaderContentWrap>
+    <PubHeaderContentWrap>
       <Link to="/users/login">
         <LoginBlock>Login</LoginBlock>
       </Link>
       <Link to="/users/signup">
         <SignupBlock>Sign up</SignupBlock>
       </Link>
-    </LoggedHeaderContentWrap>
+    </PubHeaderContentWrap>
   );
 };
 
-export const LogoBlock = styled.div`
-  padding: 10px 13px;
-  img {
-    width: 150px;
-    height: 30px;
-  }
-  :hover {
-    background-color: hsl(210, 8%, 90%);
-  }
-`;
-
 export const Header = () => {
   //로그인 여부를 확인하기 위한 상태가 필요
-  const [logged, setLogged] = useState(true);
-
-  // const modeHandler = () => {
-  //   setLogged(!logged);
-  // };
+  const [logged, setLogged] = useState(false);
 
   return (
     <>
@@ -286,7 +113,6 @@ export const Header = () => {
           <HeaderContent logged={logged} />
         </nav>
       </HeaderBlock>
-      {/* <button onClick={modeHandler}>❤️</button> */}
     </>
   );
 };
