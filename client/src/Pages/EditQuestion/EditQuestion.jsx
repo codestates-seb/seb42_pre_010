f758d2301b340c3953251711ab3d19dd79fc045d
youa7878 <dkgus7123@naver.com>
2023-02-22 15:17:09 +0900

    fix: Side 메모 부분 컴포넌트화 및 EditQuestion 불필요 코드 삭제 #89

--- client/src/Pages/EditQuestion/EditQuestion.jsx
+++ client/src/Pages/EditQuestion/EditQuestion.jsx
@@ -5,6 +5,7 @@ import {
   WritingBodyForm,
 } from '../../Components/AskForm/WritingForm';
 import EditButton from '../../Components/Button/EditButton';
+import { EditMemo } from '../../Components/SideMemo/SideMemo';
 
 const EditQuestionBlock = styled.div`
   display: flex;
@@ -30,40 +31,6 @@ const WritingFormBlock = styled.div`
   }
 `;
 
-const SideMemo = styled.div`
-  background: #fdf7e2;
-  width: 330px;
-  height: 230px;
-  border-radius: 3px;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.041), 0 0px 2px rgba(0, 0, 0, 0.096);
-  border: 1px solid #efe8cd;
-  margin: 40px 1px 1px 20px;
-
-  .memoTitle {
-    border: 1px solid #efe8cd;
-    background: #fbf3d5;
-    padding: 12px;
-    height: 16px;
-    color: #3b4045;
-    font-size: 15px;
-  }
-  ul {
-    padding-left: 30px;
-    padding: 11px 0px 11px 30px;
-  }
-
-  li {
-    list-style: disc;
-    font-size: small;
-    padding: 10px 0px 10px 0px;
-  }
-
-  li::marker {
-    color: #141414;
-    font-size: 0.7pc;
-  }
-`;
-
 const EditQuestion = () => {
   // DB에서 title, value 값 가지고 와서 초기값에 넣어 줘야 함
   const [title, setTitle] = useState('');
@@ -81,14 +48,6 @@ const EditQuestion = () => {
     window.location.href = './questions';
   };
 
-  const memoText = [
-    'Correct minor typos or mistakes',
-    'Clarify meaning without changing it',
-    'Add related resources or links',
-    'Always respect the author’s intent',
-    'Don’t use edits to reply to the author',
-  ];
-
   return (
     <EditQuestionBlock>
       <EditQuestionContainer>
@@ -101,14 +60,7 @@ const EditQuestion = () => {
           <h3 className="title">Body</h3>
           <WritingBodyForm value={value} handleValue={handleValue} />
         </WritingFormBlock>
-        <SideMemo>
-          <div className="memoTitle">How to Edit</div>
-          <ul>
-            {memoText.map((text, idx) => (
-              <li key={idx}>{text}</li>
-            ))}
-          </ul>
-        </SideMemo>
+        <EditMemo />
       </EditQuestionContainer>
       <EditButton handleOnCancel={handleOnCancel} />
     </EditQuestionBlock>
