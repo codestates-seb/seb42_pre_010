73f00bb3a0caf4ac284198611fa8126403b98fd6
dooyong2 <asdffg725@naver.com>
2023-02-24 04:19:26 +0900

    conflicts수정 및 전체 코드 수정#107

--- server/src/main/java/com/seb10/server/domain/question/entity/Question.java
+++ server/src/main/java/com/seb10/server/domain/question/entity/Question.java
@@ -24,8 +24,7 @@ public class Question {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long questionId;
 
-
-    @Column(nullable = false, length = 200)
+    @Column(nullable = false, length = 50)
     private String title;
 
     @Column(nullable = false)
@@ -36,11 +35,11 @@ public class Question {
     private QuestionStatus questionStatus = QuestionStatus.QUESTION_NOSELECT;
 
     @CreatedDate
-    @Column(name = "question_created_at")
+    @Column(nullable = false, name = "question_created_at")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(name = "question_last_modified_at")
+    @Column(nullable = false, name = "question_last_modified_at")
     private LocalDateTime modifiedAt;
 
     // user 매핑
@@ -48,15 +47,27 @@ public class Question {
     @JoinColumn(name = "USER_ID")
     private User user;
 
+    public void setUser(User user) {
+        this.user = user;
+        if (!this.user.getQuestion().answers(this)) {
+            this.user.getQuestion().add(this);
+        }
+    }
+
+    public Question(User user) {
+        this.user = user;
+    }
+
+
     // answer 매핑
     @OneToMany(mappedBy = "question", cascade = {CascadeType.PERSIST, CascadeType.REFRESH})
     private List<Answer> answers = new ArrayList<>();
 
     public void setAnswer(Answer answer) {
         answers.add(answer);
-//        if (answer.getQuestion() != this) {
-//            answer.setQuestion(this);
-//        }
+        if (answer.getQuestion() != this) {
+            answer.setQuestion(this);
+        }
     }
 
 }
