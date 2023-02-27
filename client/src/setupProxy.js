87d8322c632dc02bdb79dc9e68dd7dde40389aa7
suming <ipower_overi@naver.com>
2023-02-27 16:03:59 +0900

    refactor: Proxy API_URL 환경 변수 등록

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '*',
     createProxyMiddleware({
-      target: 'https://6cad-218-50-40-149.jp.ngrok.io',
+      target: process.env.REACT_APP_API_URL,
       changeOrigin: true,
     })
   );
